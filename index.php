682dfcfb15d0e0bc1a6e2097c4603924dad51951
Matt Carlucci <mcarlucci@wyndhamjade.com>
2013-11-25 01:21:02 -0600

    added db connection

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
-  // we will fill this out next
+  return "dbname=dpdgs36tdn14a host=ec2-54-221-206-165.compute-1.amazonaws.com port=5432 user=iklzsflimhwbbh password=K1DuYxivXcBvwAkA6TA6qjhnjX sslmode=require"
 }
  
 # Establish db connection
